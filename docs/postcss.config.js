2bfe6e4c6c6824d1a5acd52f1938f6601aab186a
Kirill Lebedev <lebedev@gmail.com>
2021-09-06 11:28:04 -0700

    Initial layout

---
+++ docs/postcss.config.js
@@ -0,0 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        // Jekyll output directory
+        './_site/**/*.html',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+    purge: false,
+    plugins: [
+        require('tailwindcss'),
+        require('cssnano')(),
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+    ]
+};
